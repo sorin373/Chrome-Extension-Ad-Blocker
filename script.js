898a9af8e37458b9f70ac9deb11e9065b5fa64e5
Tudose Sorin <sorin.andrei.tudose@gmail.com>
2024-07-14 16:41:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ setInterval( function() {
     // homepage ads
     let adDiv_1 = document.getElementsByClassName("ytd-ad-slot-renderer");
     let adDiv_2 = document.getElementsByClassName("ytd-display-ad-renderer");
+    let adDiv_3 = document.getElementsByClassName("ytd-player-legacy-desktop-watch-ads-renderer");
 
     if (adDiv_1.length > 0 && adDiv_1 != undefined)
 	for (let i = 0; i < adDiv_1.length; i++)
@@ -11,6 +12,10 @@ setInterval( function() {
     if (adDiv_2.length > 0 && adDiv_2 != undefined)
 	for (let i = 0; i < adDiv_2.length; i++)
             adDiv_2[i].style.display = 'none';
+
+    if (adDiv_3.length > 0 && adDiv_3 != undefined)
+        for (let i = 0; i < adDiv_3.length; i++)
+            adDiv_3[i].style.display = 'none';
 
     let skipBtn = document.getElementsByClassName("ytp-skip-ad-button");
     
